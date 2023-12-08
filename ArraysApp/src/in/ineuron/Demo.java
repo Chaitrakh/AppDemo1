71a73f292c15e9759996f33fdac58cc2e7ac4b55
chaitra <chaitrakh94@gmail.com>
2023-12-08 23:41:48 +0530

    This is first commit

---
+++ ArraysApp/src/in/ineuron/Demo.java
@@ -0,0 +1,13 @@
+package in.ineuron;
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+		int[] a= {10,20,30,40,50};
+		for(int ele:a) {
+			System.out.println(ele);
+		}
+	}
+
+}
